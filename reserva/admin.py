bd0b4bc92acdaa9c6b510f9cf9c062442146d809
Chopperkasa <limaszandresfelipe@gmail.com>
2023-05-02 20:04:38 -0500

    Se modificaron los archivos admin para administrar los datos desde la pagina de admin

--- reserva/admin.py
+++ reserva/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Sala, Funcion, Reserva, Venta
 
-# Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Sala)
+admin.site.register(Funcion)
+admin.site.register(Reserva)
+admin.site.register(Venta)
