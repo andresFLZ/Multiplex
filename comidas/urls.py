604835827a735cb36ed9eda7dc12de56f055429a
Chopperkasa <limaszandresfelipe@gmail.com>
2023-05-16 20:19:50 -0500

    Se implemento el crud para comidas

--- comidas/urls.py
+++ comidas/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from comidas.views import ComidasCreate, ComidasList, ComidasDelete, ComidasUpdate
+from comidas.views import Comida
 
-app_name = 'comidas'
+app_name = 'comida'
 urlpatterns = [
-    path('', ComidasList.as_view(), name='List'),
-    path('nuevo/', ComidasCreate.as_view(), name='Create'),
-    path('editar/<int:pk>/', ComidasUpdate.as_view(), name='Update'),
-    path('eliminar/<int:pk>/', ComidasDelete.as_view(), name='Delete'),
+    path('', Comida.as_view(), name='comida'),
 ]
