051a83c29d917f894d70873c1325cbdcdc49bcf7
Chopperkasa <limaszandresfelipe@gmail.com>
2023-05-31 09:42:17 -0500

    El usuario puede ver sus reservas

--- reserva/urls.py
+++ reserva/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from reserva.views import ReservaRedirect, reservaConfirm
+from reserva.views import ReservaRedirect, ReservaConfirm, ReservaList
 
 app_name = 'reserva'
 urlpatterns = [
-    path('confirmacion/', reservaConfirm.as_view(), name='Confirm'),
+    path('reservas/', ReservaList.as_view(), name='List'),
+    path('confirmacion/', ReservaConfirm.as_view(), name='Confirm'),
     path('', ReservaRedirect.as_view(), name='Reserva'),
 ]
